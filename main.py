05f8fb91a1034f26e3fa183d8e4c342f05265ad6
Sckathach <sckathach@hackademint.org>
2024-04-11 14:28:41 +0200

    First results

--- main.py
+++
@@ -1,8 +0,0 @@
-import networkx as nx
-import matplotlib.pyplot as plt
-
-# Load the Caltech network
-G_caltech = nx.read_gml('path_to_caltech.gml')
-
-# Print basic information to verify it's loaded correctly
-print(nx.info(G_caltech))
